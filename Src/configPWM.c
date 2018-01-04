d5958cba661ac30cc76122c44851235b2f03007a
Mayeul Cantan <mayeul.cantan@gmail.com>
2018-01-04 17:08:00 +0100

    Add copyright headers

--- Src/configPWM.c
+++ Src/configPWM.c
@@ -1,3 +1,20 @@
+/*
+ *   configPWM.c contains the PWM configuration functions
+ *   Copyright (C) 2017  Mayeul Cantan <mayeul.cantan@gmail.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "configPWM.h"
 #include "stm32f4xx_hal.h"
 #include "tim.h"
